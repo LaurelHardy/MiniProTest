573b789e045bba782a9b1b952b9d14565b0ee710
MinGW64 <justinyu2012@hotmail.com>
2024-08-19 17:14:00 +0800

    轮播图完成

--- components/Recommend/Recommend.vue
+++ components/Recommend/Recommend.vue
@@ -1,6 +1,22 @@
 <template>
-	<view>
-		Recommend
+	<view class="recommend">
+		<swiper class="swiper" :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+			<swiper-item>
+				<view class="swiper-item">
+					<image src="https://img3.doubanio.com/view/photo/l/public/p2369211952.webp" mode=""></image>
+				</view>
+			</swiper-item>
+			<swiper-item>
+				<view class="swiper-item">
+					<image src="https://tse4-mm.cn.bing.net/th/id/OIP-C.iuDzZlquUTm5weyA7AoMwAHaEL?w=290&h=181&c=7&r=0&o=5&pid=1.7 mode=""></image>
+				</view>
+			</swiper-item>
+			<swiper-item>
+				<view class="swiper-item">
+					<image src="https://tse4-mm.cn.bing.net/th/id/OIP-C.6FBx1qKqBllpAZFqeb5nTQHaEH?w=257&h=180&c=7&r=0&o=5&pid=1.7" mode=""></image>
+				</view>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -15,5 +31,10 @@
 </script>
 
 <style lang="stylus">
-
+	.recommend
+		.swiper
+			height: 350rpx
+			image
+				width: 100%
+				height: 350rpx
 </style>
