c009c7001373dc841db56fd35d45651de90845ff
MinGW64 <justinyu2012@hotmail.com>
2024-08-20 14:29:38 +0800

    分类模块区

--- components/Recommend/Recommend.vue
+++ components/Recommend/Recommend.vue
@@ -33,6 +33,20 @@
 				<view class="kingKongDesc"> {{item.text}} </view>
 			</view>
 		</view>
+		<!-- 4、分类区 数据: categoryModule -->
+		<view class="categoryList">
+			<view class="categoryItem" v-for="item in indexData.categoryModule" :key="item.titlePicUrl">
+				<!-- 大图 -->
+				<image class="categoryImg" :src="item.titlePicUrl" mode=""></image>
+				<!-- 滑块 -->
+				<scroll-view class="categoryScroll" scroll-x="true" enable-flex>
+					<view class="goodsItem" v-for="goodsItem in item.itemList" :key="goodsItem.id">
+						<image class="goodsImg" :src="goodsItem.showPicUrl" mode=""></image>
+						<view class="goodsText">{{goodsItem.name}}</view>
+					</view>
+				</scroll-view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -84,4 +98,31 @@
 					height: 100rpx
 				.kingKongDesc
 					font-size: 25rpx
+		.categoryList
+			.categoryItem
+				margin: 10rpx 0
+				.categoryImg
+					height: 370rpx
+					width: 100%
+				.categoryScroll
+					// 注意：一定给滑块设置高度，否则撑满全屏
+					height: 300rpx
+					display: flex
+					//不换行
+					white-space: nowrap
+					.goodsItem
+						height: 300rpx						
+						margin: 0 5rpx
+						.goodsImg
+							width: 200rpx
+							height: 200rpx
+							background-color: #f5f5f5
+						.goodsText
+							// 换行，超出2行...
+							white-space: pre-wrap
+							overflow: hidden
+							text-overflow: ellipsis
+							display: -webkit-box
+							-webkit-box-orient: vertical
+							-webkit-line-clamp: 2
 </style>
